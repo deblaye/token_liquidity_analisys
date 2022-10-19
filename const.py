35bac394bdff50f25a2fafc631f35ac451961e9a
louis-de-bontin <louis@newlogic.com>
2022-10-19 21:09:52 +0700

    Started collecting data

---
+++ const.py
@@ -0,0 +1,18 @@
+symbols = {
+    "BTC": "bitcoin",
+    "UNI": "uniswap",
+    "XMR": "monero",
+    "VET": "vechain",
+    "USDT": "tether",
+    "BNB": "binancecoin",
+    "ETH": "ethereum",
+    "MATIC": "matic-network",
+    "DOT": "polkadot",
+    "DOGE": "dogecoin",
+    "XRP": "ripple",
+    "SAND": "the-sandbox",
+    "MANA": "decentraland",
+    "SUSHI": "sushi"
+}
+
+pairs = {}
